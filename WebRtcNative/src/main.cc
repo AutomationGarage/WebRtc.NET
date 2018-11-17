c47e55a9ddd2f02b26e4d3e1b8e9cb34ad0309de
Denis Anchugov <denis.anchugov@outlook.com>
2018-11-17 23:56:33 +0200

    Removed libs that no longer needed. Everything needed seems to be properly linked into webrtc.lib.

--- WebRtcNative/src/main.cc
+++ WebRtcNative/src/main.cc
@@ -6,17 +6,6 @@
 #pragma comment(lib,"msdmo.lib")
 #pragma comment(lib,"Strmiids.lib")
 
-#pragma comment(lib,"bbr.lib")
-#pragma comment(lib,"bandwidth_sampler.lib")
-#pragma comment(lib,"bbr_controller.lib")
-#pragma comment(lib,"loss_rate_filter.lib")
-#pragma comment(lib,"rtt_stats.lib")
-
-#pragma comment(lib,"create_pc_factory.lib")
-#pragma comment(lib,"peerconnection.lib")
-#pragma comment(lib,"builtin_video_decoder_factory.lib")
-#pragma comment(lib,"builtin_video_encoder_factory.lib")
-
 #pragma comment(lib,"webrtc.lib")
 
 #include "internals.h"
